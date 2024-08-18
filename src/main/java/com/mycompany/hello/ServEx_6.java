310972cd7599d30d2d3accc09df5857e4e494d7d
avm-x <av-mmi@outlook.com>
2024-08-18 08:08:50 -0400

    fixed the displayer of current products in store

--- src/main/java/com/mycompany/hello/ServEx_6.java
+++ src/main/java/com/mycompany/hello/ServEx_6.java
@@ -12,12 +12,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ServEx_6 extends HttpServlet {
-    private boolean tracker;
+    private ResultSet rsNProducts;
+
     public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
-        long productsNum = 0;
         PrintWriter out = res.getWriter();
         
+        out.println("<HTML>");
+        out.println("<HEAD>");
+        out.println("<link rel='stylesheet' href='./css/servex_6.css'>");
+        out.println("<TITLE>ServEx_6</TITLE>");
+        out.println("</HEAD>");
+        out.println("<BODY>");
+        out.println("<h1>ServEx_6: Store and retrieve products</h1>");
+
         try{
             String jdbcUrl = "jdbc:postgresql://localhost:5432/servexdb";
             String username = "postgres";
@@ -29,28 +37,30 @@ public class ServEx_6 extends HttpServlet {
             Statement statement = connection.createStatement();
 
             // if table exist update number of products
-            if(doesTableExist(connection, "products")){
-                ResultSet retrieveRecords = statement.executeQuery("SELECT COUNT(*) FROM products;");
-                productsNum = retrieveRecords.getLong(1);
-                tracker = doesTableExist(connection, "products");
-            } else {
+            if(!doesTableExist(connection, "products")){
                 int rowsAffected = statement.executeUpdate("CREATE TABLE products (code INTEGER, name TEXT, weight FLOAT, type TEXT);");
-                connection.commit();
-                tracker = doesTableExist(connection, "products");
             }
-        } catch(Exception E){
+            
+            rsNProducts = statement.executeQuery("SELECT COUNT(*) FROM products");
+
+            connection.commit();
+        } catch(Exception e){
+            out.println("There was an error while trying to create table: " + e.getMessage());
         }
 
-        out.println("<HTML>");
-        out.println("<HEAD>");
-        out.println("<link rel='stylesheet' href='./css/servex_6.css'>");
-        out.println("<TITLE>ServEx_6</TITLE>");
-        out.println("</HEAD>");
-        out.println("<BODY>");
-        out.println("<h1>ServEx_6: Store and retrieve products</h1>");
         out.println("  <div class='displayer-cont'><div class='d-head'><h2>Product form</h2></div><div class='d-body'><form method='post' name='productForm' action='http://localhost:8080/servlets-exercises/servEx_6/handling-form'><div class='input-cont'><label for='code'>code<label><input type='text' name='code' id='code' placeholder='0000' maxlength='4'></div><div class='input-cont'><label for='name'>name<label><input type='text' name='name' id='name' placeholder='candy box'></div><div class='input-cont'><label for='weight'>weight(in grams)<label><input type='text' name='weight' id='weight' placeholder='500'></div><div class='input-cont'><label for='type'>type<label><input type='text' name='type' id='type' placeholder='food'></div><input type='submit' value='add product'></form></div><div class='dfooter'><span>Current products in store: </span>");
-        out.println("<span>" + productsNum + "</span>" + "<br/><span>Take into account that only unique products (in base to their code) are gonna be stored!</span>" + "</div></div>");
-        out.println("Does table exist? " + tracker);
+
+        try {
+
+            if(rsNProducts != null && rsNProducts.next()){
+                out.println("<span>"+ rsNProducts.getInt(1) +"</span>");
+            }
+            
+        } catch (Exception e) {
+            out.println("There was an error retrieving the count of products " + e.getMessage());
+        }
+
+        out.println("<br/><span>Take into account that only unique products (in base to their code) are gonna be stored!</span>" + "</div></div>");
         out.println("</BODY>");
         out.println("</HTML>");
     }
